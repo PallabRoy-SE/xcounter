ac5de9e726e720cebc9323d02e8773bb85a2ce49
PALLAB ROY <76547199+PallabRoy-SE@users.noreply.github.com>
2024-05-10 17:14:55 +0530

    feat: add counter functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-    return <></>;
+    const [count, setCount] = useState(0);
+    return (
+        <section style={{ marginLeft: '1.5rem' }}>
+            <h1>Counter App</h1>
+            <p style={{ marginBlock: '1.5rem' }}>Count: {count}</p>
+            <div>
+                <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+                <button onClick={() => setCount((prev) => prev - 1)}>Decrement</button>
+            </div>
+        </section>
+    );
 }
 
 export default App;
